b0f204dcd11f07e8e0bccf3009a4aacee912c2ef
deathKiller10 <priyanshuwork.10@gmail.com>
2024-04-19 00:19:02 +0530

    Added a display to display the tables as and when I wish

--- index.js
+++ index.js
@@ -15,6 +15,37 @@ document.getElementById("credentials").addEventListener("submit", function(event
 
     records.push(Employee);
 
+    // let table = document.getElementById("myTable");
+    // let row = document.createElement("tr");
+    // let cell1 = document.createElement("td");
+    // let cell2 = document.createElement("td");
+    // let cell3 = document.createElement("td");
+
+    // // Delete Button
+    // let cell4 = document.createElement("td");
+    // let lastColumnButton = document.createElement("button");
+    // lastColumnButton.classList.add("deleteButton");
+    // lastColumnButton.innerText = "Delete";
+
+    // cell1.innerText = name;
+    // cell1.classList.add("tableColumn", "firstColumn");
+    // cell2.innerText = position;
+    // cell2.classList.add("tableColumn");
+    // cell3.innerText = salary;
+    // cell3.classList.add("tableColumn", "secondLastColumn");
+    // cell4.id = "lastColumn";
+    // cell4.appendChild(lastColumnButton);
+
+    // row.appendChild(cell1);
+    // row.appendChild(cell2);
+    // row.appendChild(cell3);
+    // row.appendChild(cell4);
+
+    // table.appendChild(row);
+});
+
+document.querySelector(".displayTable").addEventListener("click", function(){
+    console.log("Displaying table...");
     let table = document.getElementById("myTable");
     let row = document.createElement("tr");
     let cell1 = document.createElement("td");
@@ -27,11 +58,11 @@ document.getElementById("credentials").addEventListener("submit", function(event
     lastColumnButton.classList.add("deleteButton");
     lastColumnButton.innerText = "Delete";
 
-    cell1.innerText = name;
+    cell1.innerText = records[records.length - 1]["name"];
     cell1.classList.add("tableColumn", "firstColumn");
-    cell2.innerText = position;
+    cell2.innerText = records[records.length - 1]["position"];
     cell2.classList.add("tableColumn");
-    cell3.innerText = salary;
+    cell3.innerText = records[records.length - 1]["salary"];
     cell3.classList.add("tableColumn", "secondLastColumn");
     cell4.id = "lastColumn";
     cell4.appendChild(lastColumnButton);
@@ -42,5 +73,4 @@ document.getElementById("credentials").addEventListener("submit", function(event
     row.appendChild(cell4);
 
     table.appendChild(row);
-});
-
+})
