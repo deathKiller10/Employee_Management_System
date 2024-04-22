7f2625ab5fa42739f0f3db43f9b390218a7babd8
deathKiller10 <priyanshuwork.10@gmail.com>
2024-04-22 20:10:37 +0100

    added delete feature for rows

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-let records = [];
+let isAdded = [];
+let isRemoved = [];
+let x = 0;
+let rowIndex = 0;
 
 document.getElementById("credentials").addEventListener("submit", function(event){
     event.preventDefault();
@@ -13,41 +16,14 @@ document.getElementById("credentials").addEventListener("submit", function(event
         salary: salary
     }
 
-    records.push(Employee);
+    isAdded.push(Employee);
 
-    // let table = document.getElementById("myTable");
-    // let row = document.createElement("tr");
-    // let cell1 = document.createElement("td");
-    // let cell2 = document.createElement("td");
-    // let cell3 = document.createElement("td");
-
-    // // Delete Button
-    // let cell4 = document.createElement("td");
-    // let lastColumnButton = document.createElement("button");
-    // lastColumnButton.classList.add("deleteButton");
-    // lastColumnButton.innerText = "Delete";
-
-    // cell1.innerText = name;
-    // cell1.classList.add("tableColumn", "firstColumn");
-    // cell2.innerText = position;
-    // cell2.classList.add("tableColumn");
-    // cell3.innerText = salary;
-    // cell3.classList.add("tableColumn", "secondLastColumn");
-    // cell4.id = "lastColumn";
-    // cell4.appendChild(lastColumnButton);
-
-    // row.appendChild(cell1);
-    // row.appendChild(cell2);
-    // row.appendChild(cell3);
-    // row.appendChild(cell4);
-
-    // table.appendChild(row);
-});
-
-document.querySelector(".displayTable").addEventListener("click", function(){
-    console.log("Displaying table...");
+    // table
     let table = document.getElementById("myTable");
-    let row = document.createElement("tr");
+    let row = document.createElement("tr"); // row
+    row.id = `row${++rowIndex}`;
+
+    // 3 columns
     let cell1 = document.createElement("td");
     let cell2 = document.createElement("td");
     let cell3 = document.createElement("td");
@@ -55,14 +31,14 @@ document.querySelector(".displayTable").addEventListener("click", function(){
     // Delete Button
     let cell4 = document.createElement("td");
     let lastColumnButton = document.createElement("button");
-    lastColumnButton.classList.add("deleteButton");
+    lastColumnButton.classList.add("deleteButton", `button${++x}`);
     lastColumnButton.innerText = "Delete";
 
-    cell1.innerText = records[records.length - 1]["name"];
+    cell1.innerText = name;
     cell1.classList.add("tableColumn", "firstColumn");
-    cell2.innerText = records[records.length - 1]["position"];
+    cell2.innerText = position;
     cell2.classList.add("tableColumn");
-    cell3.innerText = records[records.length - 1]["salary"];
+    cell3.innerText = salary;
     cell3.classList.add("tableColumn", "secondLastColumn");
     cell4.id = "lastColumn";
     cell4.appendChild(lastColumnButton);
@@ -73,4 +49,20 @@ document.querySelector(".displayTable").addEventListener("click", function(){
     row.appendChild(cell4);
 
     table.appendChild(row);
-})
+});
+
+
+document.addEventListener('click', (event) => {
+    let table = document.getElementById("myTable");
+    if (event.target.classList.contains('deleteButton')) {
+        const index = parseInt(event.target.classList[1].slice(6)); //Extract the index from the class
+        isRemoved.push(isAdded[index-1]);
+        console.log(isRemoved);
+        
+        const tableRow = event.target.closest('tr');
+        // console.log(tableRow.id);
+        // console.log(tableRow.rowIndex - 1);
+        table.deleteRow(tableRow.rowIndex);
+    }
+});
+
