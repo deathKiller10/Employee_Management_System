e90c2b8d398e6f41b28ff3cb97c7ca1fbbe4c4ef
deathKiller10 <priyanshuwork.10@gmail.com>
2024-04-27 17:49:03 +0100

    Half of edit functionality done, along with some styling changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // onclick="location.href='./search.html'"
+// onclick="location.href='./edit.html'"
 
 let isAdded = [], isRemoved = [];
 let x = 0, rowIndex = 0;
@@ -73,28 +74,84 @@ document.addEventListener('click', (event) => {
     }
 });
 
+// searching an employee
 document.querySelector("#searchEmployee").addEventListener("click", function(){
     document.querySelector(".searching").classList.remove("searchEmployeeHidden");
     document.querySelector(".adding").classList.add("containerHidden");
+    if (!document.querySelector(".editing").classList.contains("editingHidden")){
+        document.querySelector(".editing").classList.add("editingHidden");
+    }
+    if (document.querySelector(".display").classList.contains("displayHidden")){
+        document.querySelector(".display").classList.remove("displayHidden");
+    }
+    document.querySelectorAll("#lastColumn").forEach((element) => {
+        element.style.display = "none";
+    });
 
     let table = document.querySelector(".display");
     let parent = table.parentNode;
     parent.insertBefore(document.querySelector(".searching"), table);
+
+    // Searching an employee based on name and position
+    document.querySelector(".searchInput").addEventListener("input", function(event){
+        event.preventDefault();
+        const filter = this.value.toLowerCase();
+        const rows = document.getElementById("myTable").rows;
+
+        for (let i = 1; i < rows.length; i++) {
+            const name = rows[i].cells[0].textContent.toLowerCase();
+            const position = rows[i].cells[1].textContent.toLowerCase();
+            if (name.includes(filter) || position.includes(filter)){
+                rows[i].style.display = "";
+            } else {
+                rows[i].style.display = "none";
+            }
+        }
+    });
 });
 
-// Searching an employee based on name and position
-document.querySelector(".searchInput").addEventListener("input", function(event){
-    event.preventDefault();
-    const filter = this.value.toLowerCase();
-    const rows = document.getElementById("myTable").rows;
-
-    for (let i = 1; i < rows.length; i++) {
-        const name = rows[i].cells[0].textContent.toLowerCase();
-        const position = rows[i].cells[1].textContent.toLowerCase();
-        if (name.includes(filter) || position.includes(filter)){
-            rows[i].style.display = "";
-        } else {
-            rows[i].style.display = "none";
-        }
+// Editing an employee's details
+document.getElementById("editEmployee").addEventListener("click", function(){
+    document.querySelector(".editing").classList.remove("editingHidden");
+    document.querySelector(".adding").classList.add("containerHidden");
+    if (!document.querySelector(".display").classList.contains("displayHidden")){
+        document.querySelector(".display").classList.add("displayHidden");
     }
+    console.log(document.querySelector(".display").classList);
+
+    if(!document.querySelector(".searching").classList.contains("searchEmployeeHidden")){
+        document.querySelector(".searching").classList.add("searchEmployeeHidden");
+    }
+    document.getElementById("searchForEdit").addEventListener("submit", function(event){
+        event.preventDefault();
+        let name = document.getElementById("search").value.toLowerCase();
+        let flag = 0;
+        for (let i = 0; i < isAdded.length; i++) {
+            if (isAdded[i].name.toLowerCase() === name) {
+                flag = 1;
+                rowIndex = i;
+                break;
+            }
+        }
+        if (flag === 1) {
+            console.log("Employee is present!");
+            document.querySelector(".editEmployee").classList.remove("editEmployeeHidden");
+        } else {
+            console.log("Employee is not present!");
+            alert("Employee is not present!");
+        }
+
+        // When updated details are entered and save button is clicked
+        document.getElementById("saveChanges").addEventListener("click", function(event){
+            event.preventDefault();
+            let newPosition = document.getElementById("newPosition").value;
+            let newSalary = parseInt(document.getElementById("newSalary").value);
+            isAdded[rowIndex].position = newPosition;
+            isAdded[rowIndex].salary = newSalary;
+            // document.querySelector(".editEmployee").classList.add("editEmployeeHidden");
+            // if (document.querySelector(".display").classList.contains("displayHidden")){
+            //     document.querySelector(".display").classList.remove("displayHidden");
+            // }
+        })
+    })
 });
